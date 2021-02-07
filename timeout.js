b962a02c46c16f8643a31094c4a2ba9ef7897dc0
saidur1529 <saidur.rahman04@northsouth.edu>
2021-02-07 11:54:57 +0600

    timout function

---
+++ timeout.js
@@ -0,0 +1,14 @@
+function doSomething () { 
+    console.log(3333);
+}
+console.log(2222);
+setTimeout(()=> {
+    console.log('lazy and waiting');
+}, 5);
+// setInterval(() => {
+//     console.log('Doing it')
+// }, 3000);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
